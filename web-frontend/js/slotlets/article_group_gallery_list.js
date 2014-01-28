2c84f28bc5163a51c0425847bd1414204afe708f
Juan Pablo <jpablop@cespi.unlp.edu.ar>
2014-01-28 13:30:52 -0300

    Se agrega un nuevo slotlet para mostrar varias galerias

---
+++ web-frontend/js/slotlets/article_group_gallery_list.js
@@ -0,0 +1,25 @@
+jQuery(function($){
+    //when a link in the filters div is clicked...
+    $('.article_group_gallery_filters a').click(function(e){
+        var container = $(this).closest('.article_group_gallery_container');
+
+        console.log(container);
+        console.dir(container.find('li'));
+
+        //prevent the default behaviour of the link
+        e.preventDefault();
+
+        //get the id of the clicked link(which is equal to classes of our content
+        var filter = $(this).attr('id');
+
+        //show all the list items(this is needed to get the hidden ones shown)
+        container.find('li').show();
+
+        /*using the :not attribute and the filter class in it we are selecting
+         only the list items that don't have that class and hide them '*/
+        container.find('li:not(.' + filter + ')').hide();
+
+    }).first().trigger('click');
+
+
+});
